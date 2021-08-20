cfa273a17b3de4c4ace3abbc6d1dbfb36ea18986
Jiaxuan Wang <jiaxuanw1@gmail.com>
2021-08-20 10:07:54 -0400

    Use default sleep method when drawing fractals

--- src/fractals/DragonCurve.java
+++ src/fractals/DragonCurve.java
@@ -7,52 +7,52 @@ import util.Turtle;
 
 public class DragonCurve extends Turtle {
 
-    public static void main(String[] args) {
-        DragonCurve fractal = new DragonCurve();
-        fractal.drawDragon(16, 1);
-    }
+	public static void main(String[] args) {
+		DragonCurve fractal = new DragonCurve();
+		fractal.drawDragon(16, 1);
+	}
 
-    /**
-     * Draws a dragon curve to the screen with a specified side length and number of
-     * iterations.
-     * 
-     * @param iterations the number of iterations
-     * @param sideLength the length of each line segment, in pixels
-     */
-    public void drawDragon(int iterations, int sideLength) {
-        // Create a list to store the sequence of turns (as 0's and 1's)
-        List<Integer> sequence = new ArrayList<Integer>();
+	/**
+	 * Draws a dragon curve to the screen with a specified side length and number of
+	 * iterations.
+	 * 
+	 * @param iterations the number of iterations
+	 * @param sideLength the length of each line segment, in pixels
+	 */
+	public void drawDragon(int iterations, int sideLength) {
+		// Create a list to store the sequence of turns (as 0's and 1's)
+		List<Integer> sequence = new ArrayList<Integer>();
 
-        // For every iteration, add a 1 to the sequence, then add the reverse of the
-        // previous sequence with 0's and 1's swapped.
-        for (int i = 0; i < iterations; i++) {
-            List<Integer> inverse = reverseInverseSequence(sequence);
-            sequence.add(0);
-            sequence.addAll(inverse);
-        }
+		// For every iteration, add a 1 to the sequence, then add the reverse of the
+		// previous sequence with 0's and 1's swapped.
+		for (int i = 0; i < iterations; i++) {
+			List<Integer> inverse = reverseInverseSequence(sequence);
+			sequence.add(0);
+			sequence.addAll(inverse);
+		}
 
-        // Move into position so the fractal is in frame
-        move(90, -100);
-        // 0's represent right turns and 1's represent left turns
-        for (int turn : sequence) {
-            paint(turn == 0 ? -90 : 90, sideLength);
-        }
-    }
+		// Move into position so the fractal is in frame
+		move(90, -100);
+		// 0's represent right turns and 1's represent left turns
+		for (int turn : sequence) {
+			paint(turn == 0 ? -90 : 90, sideLength);
+		}
+	}
 
-    /**
-     * Reverses a specified sequence of 0's and 1's and inverts each number (so 0's
-     * become 1's and vice versa). Each element in the list must be a single digit
-     * (0 or 1).
-     * 
-     * @param sequence list containing the sequence of 0's and 1's
-     * @return list containing the reversed inverse sequence of 0's and 1's
-     */
-    public List<Integer> reverseInverseSequence(List<Integer> sequence) {
-        List<Integer> newSequence = new ArrayList<Integer>();
-        for (int i = sequence.size() - 1; i >= 0; i--) {
-            newSequence.add(sequence.get(i) == 0 ? 1 : 0);
-        }
-        return newSequence;
-    }
+	/**
+	 * Reverses a specified sequence of 0's and 1's and inverts each number (so 0's
+	 * become 1's and vice versa). Each element in the list must be a single digit
+	 * (0 or 1).
+	 * 
+	 * @param sequence list containing the sequence of 0's and 1's
+	 * @return list containing the reversed inverse sequence of 0's and 1's
+	 */
+	public List<Integer> reverseInverseSequence(List<Integer> sequence) {
+		List<Integer> newSequence = new ArrayList<Integer>();
+		for (int i = sequence.size() - 1; i >= 0; i--) {
+			newSequence.add(sequence.get(i) == 0 ? 1 : 0);
+		}
+		return newSequence;
+	}
 
 }
--- src/fractals/TreeFractal.java
+++ src/fractals/TreeFractal.java
@@ -1,60 +1,52 @@
 package fractals;
 
-import java.util.concurrent.TimeUnit;
-
 import util.Turtle;
 
 public class TreeFractal extends Turtle {
 
-    private int min = 4;
+	private int min = 1;
 
-    public static void main(String[] args) {
-        TreeFractal fractal = new TreeFractal();
-        fractal.drawTree(150);
-    }
+	public static void main(String[] args) {
+		TreeFractal fractal = new TreeFractal();
+		fractal.drawTree(175);
+	}
 
-    /**
-     * Draws a branch with the specified direction and length, with all the
-     * sub-branches to create the tree fractal.
-     * 
-     * @param degrees the number of degrees to rotate counterclockwise by
-     * @param length  the length of the branch to draw, in pixels
-     */
-    public void drawBranch(int degrees, int length) {
-        // Base case - minimum branch length has been reached, so the branch is drawn as
-        // one line segment without sub-branches
-        if (length <= min) {
-            // Wait 5 ms between each stroke
-            try {
-                TimeUnit.MILLISECONDS.sleep(5);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+	/**
+	 * Draws a branch with the specified direction and length, with all the
+	 * sub-branches to create the tree fractal.
+	 * 
+	 * @param degrees the number of degrees to rotate counterclockwise by
+	 * @param length  the length of the branch to draw, in pixels
+	 */
+	public void drawBranch(int degrees, int length) {
+		// Base case - minimum branch length has been reached, so the branch is drawn as
+		// one line segment without sub-branches
+		if (length <= min) {
+			paint(degrees, length);
+			move(0, -length);
+			sleep(1);
+			return;
+		}
 
-            paint(degrees, length);
-            move(0, -length);
-            return;
-        }
+		// Draw the stem in the specified direction
+		paint(degrees, length);
+		// Draw a shorter branch angled left
+		drawBranch(35, 7 * length / 10);
+		// Draw a shorter branch angled right
+		drawBranch(-70, 7 * length / 10);
+		// Return to starting position
+		move(35, -length);
+	}
 
-        // Draw the stem in the specified direction
-        paint(degrees, length);
-        // Draw a shorter branch angled left
-        drawBranch(35, 7 * length / 10);
-        // Draw a shorter branch angled right
-        drawBranch(-70, 7 * length / 10);
-        // Return to starting position
-        move(35, -length);
-    }
-
-    /**
-     * Draws a tree fractal to the screen with a specified size.
-     * 
-     * @param size the starting length of the stem, in pixels
-     */
-    public void drawTree(int size) {
-        // Move turtle into position so the fractal is in frame and pointing north
-        move(90, -250);
-        drawBranch(0, size);
-    }
+	/**
+	 * Draws a tree fractal to the screen with a specified size.
+	 * 
+	 * @param size the starting length of the stem, in pixels
+	 */
+	public void drawTree(int size) {
+		// Move turtle into position so the fractal is in frame and pointing north
+		move(90, -250);
+		drawBranch(0, size);
+	}
 
 }
