9f33924277d164145424c598e4ccf8262b2f321e
Jiaxuan Wang <jiaxuanw1@gmail.com>
2021-09-11 16:15:57 -0400

    Add zoom on Mandelbrot set

--- src/fractals/MandelbrotSet.java
+++ src/fractals/MandelbrotSet.java
@@ -4,8 +4,8 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Point;
+import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
 
 import javax.swing.JFrame;
@@ -13,14 +13,17 @@ import javax.swing.JPanel;
 
 import org.apache.commons.math3.complex.Complex;
 
-public class MandelbrotSet extends JPanel implements MouseListener {
+public class MandelbrotSet extends JPanel {
 
-    private final int maxIterations = 255;
+    private final int maxIterations = 256;
 
     private final int width = 960;
     private final int height = 720;
-    
-    private int scale;
+
+    private int scale; // pixels per unit
+    private Complex center;
+
+    private boolean fullyDrawn;
 
     private BufferedImage image;
 
@@ -55,19 +58,30 @@ public class MandelbrotSet extends JPanel implements MouseListener {
     public MandelbrotSet() {
         this.setPreferredSize(new Dimension(width, height));
         this.setVisible(true);
-        this.addMouseListener(this);
+        this.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                Point clickPoint = e.getPoint();
+                scale *= 2;
+                center = pointToComplex(clickPoint);
 
-        scale = 1;
+                new Thread(() -> generateMandelbrot()).start();
+                new DrawThread().start();
+            }
+        });
+
+        scale = 275;
+        center = new Complex(-0.3, 0);
+
         image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
         generateMandelbrot();
     }
 
     public void generateMandelbrot() {
+        fullyDrawn = false;
         for (int x = 0; x < width; x++) {
             for (int y = 0; y < height; y++) {
-                double re = (x - width / 2.0) * 2.5 / (scale * height) - 0.4;
-                double im = (y - height / 2.0) * 2.5 / (scale * height);
-                int iterations = numIterations(new Complex(re, im));
+                int iterations = numIterations(pointToComplex(x, y));
 
                 if (iterations < maxIterations) {
                     image.setRGB(x, y, colors[iterations % colors.length].getRGB());
@@ -76,6 +90,7 @@ public class MandelbrotSet extends JPanel implements MouseListener {
                 }
             }
         }
+        fullyDrawn = true;
     }
 
     public int numIterations(Complex c) {
@@ -88,37 +103,33 @@ public class MandelbrotSet extends JPanel implements MouseListener {
         return n;
     }
 
+    public Complex pointToComplex(double x, double y) {
+        double re = (x - width / 2.0 + scale * center.getReal()) / scale;
+        double im = (y - height / 2.0 + scale * center.getImaginary()) / scale;
+        return new Complex(re, im);
+    }
+
+    public Complex pointToComplex(Point p) {
+        return pointToComplex(p.getX(), p.getY());
+    }
+
     @Override
     public void paintComponent(Graphics g) {
         g.drawImage(image, 0, 0, this);
     }
 
-    @Override
-    public void mouseClicked(MouseEvent e) {
-        Point p = e.getPoint();
-        scale *= 2;
-        generateMandelbrot();
-        repaint();
-    }
-
-    @Override
-    public void mousePressed(MouseEvent e) {
-
-    }
-
-    @Override
-    public void mouseReleased(MouseEvent e) {
-
-    }
-
-    @Override
-    public void mouseEntered(MouseEvent e) {
-
-    }
-
-    @Override
-    public void mouseExited(MouseEvent e) {
-
+    private class DrawThread extends Thread {
+        @Override
+        public void run() {
+            while (!fullyDrawn) {
+                repaint();
+                try {
+                    sleep(5);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
 }
--- src/secrets/ImageUtil.java
+++ src/secrets/ImageUtil.java
@@ -3,6 +3,7 @@ package secrets;
 import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.io.File;
+
 import javax.imageio.ImageIO;
 
 public class ImageUtil {
